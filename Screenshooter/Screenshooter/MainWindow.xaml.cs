35902fb2c44c07616be2e9e641e513971fc9f97d
Filip Leciej <leciej.filip@gmail.com>
2021-02-24 20:55:43 +0100

    Works

--- Screenshooter/Screenshooter/MainWindow.xaml.cs
+++ Screenshooter/Screenshooter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ookii.Dialogs.Wpf;
+using System.Drawing;
+using System.IO;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace Screenshooter
 {
@@ -20,9 +26,115 @@ namespace Screenshooter
     /// </summary>
     public partial class MainWindow : Window
     {
+        VistaFolderBrowserDialog ookiiDialog = new VistaFolderBrowserDialog();
+        bool startstop = false;
+        double screenLeft = SystemParameters.VirtualScreenLeft;
+        double screenTop = SystemParameters.VirtualScreenTop;
+        double screenWidth = SystemParameters.VirtualScreenWidth;
+        double screenHeight = SystemParameters.VirtualScreenHeight;
+        static string user = Environment.UserName;
+        string startpath = @"C:\Users\" + user + @"\Documents\Screenshooter\";
+        string selectpath = null;
+        string workpath = null;
+        int cojaki;
+        static int jaki = 1;
+        private  void ss()
+        {
+            
+            while (startstop == true)
+            {
+                using (Bitmap bmp = new Bitmap((int)screenWidth,
+                (int)screenHeight))
+                {
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        String filename = DateTime.Now.ToString("yyyyMMdd-hhmmss") + ".png";
+                        g.CopyFromScreen((int)screenLeft, (int)screenTop, 0, 0, bmp.Size);
+                        bmp.Save(workpath + filename);
+                    }
+                }
+                Thread.Sleep(1000 * cojaki);
+            }
+           
+
+        }
         public MainWindow()
         {
             InitializeComponent();
+            if (!Directory.Exists(startpath))
+            {
+                Directory.CreateDirectory(startpath);
+            };
+            
+        }
+        private void startShot_Click(object sender, RoutedEventArgs e)
+        {            
+            startstop = true;
+            if (selectpath == null)
+            {
+                workpath = startpath;
+            }
+            else
+            {
+                workpath = selectpath;
+            }
+            Thread thread1 = new Thread(ss);
+            cojaki = Convert.ToInt32(CoJakiCzas_Box.Text);
+            jaki = Convert.ToInt32(JakiCzas_Box.Text);
+            thread1.Start();
+            
+            //Task t = Task.Run(() => {
+            //    Task.Delay(5000).Wait();
+            //    thread1.Abort()
+            //});
+            Task.Delay(60000 * jaki).Wait();
+            thread1.Abort();
+
+
+
+
+        }
+        private void stopShot_Click(object sender, RoutedEventArgs e)
+        {
+            startstop = false;
+            
+        }
+        private void WybierzFolder_Button(object sender, RoutedEventArgs e)
+        {
+            //var openFileDialog = new OpenFileDialog();
+            //if (openFileDialog.ShowDialog() == true)
+            //{
+            //    do something with the filename
+            //    MessageBox.Show(openFileDialog.FileName);
+            //}
+
+            if (ookiiDialog.ShowDialog() == true)
+            {
+                selectpath=ookiiDialog.SelectedPath.ToString()+@"\";
+            }
         }
     }
 }
+
+
+
+
+//    private void ss()
+//{
+//    int cojaki = Convert.ToInt32(CoJakiCzas_Box.Text);
+//    while (startstop == true)
+//    {
+//        using (Bitmap bmp = new Bitmap((int)screenWidth,
+//        (int)screenHeight))
+//        {
+//            using (Graphics g = Graphics.FromImage(bmp))
+//            {
+//                String filename = DateTime.Now.ToString("yyyyMMdd-hhmmss") + ".png";
+//                g.CopyFromScreen((int)screenLeft, (int)screenTop, 0, 0, bmp.Size);
+//                bmp.Save(workpath + filename);
+//            }
+//        }
+//        Thread.Sleep(1000 * cojaki);
+//    }
+//}
+//               }).Start();
